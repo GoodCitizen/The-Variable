f8815d10eb7f5283fed056b3917cbc644fb952bf
John Peele <john@wearegoodcitizen.com>
2014-11-25 11:23:52 -0500

    Add Stellar.js, Modernizr & parallax images

--- _harp/public/assets/js/app.js
+++ _harp/public/assets/js/app.js
@@ -6,6 +6,11 @@ $(function() {
     $(this).toggleClass("is-open");
   });
 
+  // Project list parallax
+  if(!Modernizr.touch){
+    $.stellar();
+  }
+
   // Project videos
   $(".panel-video--full").click(function () {
     $(this).append('<div class="video"><iframe src="//player.vimeo.com/video/' + $(this).data('video-id') + '?title=0&amp;byline=0&amp;portrait=0&amp;wmode=opaque&amp;api=1&amp;&autoplay=1" width="1004" height="565" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe></div>');
