4bd4b838637010f45563f91c06909ca2bf67fa86
John Peele <john@wearegoodcitizen.com>
2015-01-30 12:47:19 -0500

    Rebuild project page template

--- _harp/public/static/js/app.js
+++ _harp/public/static/js/app.js
@@ -21,12 +21,12 @@ $(function() {
   }
 
   // Project videos
-  $(".panel-video--full").click(function () {
+  $(".panel-video").click(function () {
     $(this).append('<div class="video"><iframe src="//player.vimeo.com/video/' + $(this).data('video-id') + '?title=0&amp;byline=0&amp;portrait=0&amp;wmode=opaque&amp;api=1&amp;&autoplay=1" width="1004" height="565" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe></div>');
   });
 
   // Project carousels
-  $(".slides--full").slick({
+  $(".slides").slick({
     centerMode: true,
     centerPadding: '60px',
     arrows: false,
